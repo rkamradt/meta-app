b2b34eabd1020a5e94bb32c867b8099b9ade0fe0
Randal Kamradt <randysr@kamradtfamily.net>
2015-03-15 09:59:38 -0700

    update sample

--- sample/index.js
+++ sample/index.js
@@ -12,6 +12,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var metaAppFactory = require('meta-app');
+var mongoStoreFactory = require('meta-app-mongo');
+var restFactory = require('meta-app-rest');
 var morgan = require('morgan');
 
 var json = JSON.parse(fs.readFileSync('meta-data.json'));
@@ -23,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(morgan('dev', { format: 'dev', immediate: true }));
-metaApp.useMetaRest(app, metaApp.createMongoStore('User', 'mongodb://localhost:27017/myproject', 'sample'));
+restFactory(app, metaApp, mongoStoreFactory(metaApp.getModel('User'), 'mongodb://localhost:27017/myproject', 'sample'));
 
 http.createServer(app).listen(9999, function() {
  console.log('Server up: http://localhost:' + 9999);
